8566f04a83220f172b80363d4853c78e45523dea
Dan Snow <danjsnow@outlook.com>
2021-12-01 15:10:10 +0000

    Added day 1

---
+++ day_1/day_1.py
@@ -0,0 +1,20 @@
+# solution to day 1
+
+file = open("input_1a.txt", "r")
+lines = file.readlines()
+
+increasing_counter = 0
+for i in range(0,len(lines) - 2):
+    if int(lines[i]) < int(lines[i+1]):
+        increasing_counter = increasing_counter + 1
+
+print("Solution to part 1:", increasing_counter)
+
+three_window_counter = 0
+for i in range(0,(len(lines) - 4)):
+    if (int(lines[i]) + int(lines[i+1]) + int(lines[i+2]) < 
+          int(lines[i+1]) + int(lines[i+2]) + int(lines[i+3])):
+        three_window_counter = three_window_counter + 1
+
+print("Solution to part 2:", three_window_counter)
+
